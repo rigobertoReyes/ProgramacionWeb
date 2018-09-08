5ece9a18d0b8256f44ce2a8d7cfbdbc06cf5b37d
rigobertoReyes <rigoberto.reyes16@tectijuana.edu.mx>
2018-09-07 23:21:18 -0700

    Proyecto movido a la raiz,aqui solo examen teorico

--- examen1/nflp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from nflp.models import Player
-
-admin.site.register(Player)
-
--- examen1/nflp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from nflp.models import Player 
-from django.views import generic
-# Create your views here.
-class List_Player(generic.ListView):
-	template_name = "nflp/list_player.html"
-	queryset = Player.objects.all()
